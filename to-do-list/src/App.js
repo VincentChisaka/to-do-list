b631ce8413beed519c64039a7db4133970d7ab8a
vincentchisaka <vincentchisaka2@gmail.com>
2023-06-17 13:59:24 +0300

    styling

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -51,7 +51,7 @@ function deleteItem(id) {
       <ul>
         {items.map(item => {
           return(
-            <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}><strong>X</strong></button></li>
+            <li key={item.id}>{item.value} <button className='delete-button' onClick={() => deleteItem(item.id)}><strong>X</strong></button></li>
           )
         }
 
